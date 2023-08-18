3de6cfeac4290c5c259d304efe560c5ae6071938
Ricardo Arturo <49619270+Ricaidito@users.noreply.github.com>
2023-08-18 01:00:55 -0400

    Added prettier config file

---
+++ prettier.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  singleQuote: false, // Use double quotes
+  arrowParens: "avoid", // Avoid parentheses on arrow functions
+  semi: true, // Use semicolons
+  trailingComma: "es5", // Use trailing commas
+};
