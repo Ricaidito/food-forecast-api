42c579fe51fbf28856d5535900349a8857518c92
Ricardo Arturo <ricardoarturo2707@gmail.com>
2023-11-11 00:28:49 -0400

    Added subscription

--- src/controllers/payments.controller.js
+++ src/controllers/payments.controller.js
@@ -1,19 +1,35 @@
+const User = require("../models/user.model");
 const Stripe = require("stripe");
 const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
 
-const createPaymentIntent = async (req, res) => {
-  try {
-    const { amount } = req.body;
-    const paymentIntent = await stripe.paymentIntents.create({
-      amount,
-      currency: "usd",
-    });
-    res.send({
-      clientSecret: paymentIntent.client_secret,
-    });
-  } catch (error) {
-    res.status(400).send(error.message);
-  }
+const createCustomer = async (req, res) => {
+  const user = await User.findById(req.params.userId);
+  const customer = await stripe.customers.create({
+    email: user.email,
+    name: `${user.name} ${user.lastName}`,
+  });
+  res.send({ customerId: customer.id });
 };
 
-module.exports = { createPaymentIntent };
+const createSubscription = async (req, res) => {
+  const { customerId, paymentMethodId } = req.body;
+
+  await stripe.paymentMethods.attach(paymentMethodId, {
+    customer: customerId,
+  });
+
+  await stripe.customers.update(customerId, {
+    invoice_settings: {
+      default_payment_method: paymentMethodId,
+    },
+  });
+
+  const subscription = await stripe.subscriptions.create({
+    customer: customerId,
+    items: [{ plan: "price_1OAyNGLxWE0zVARfRMTOiG3l" }],
+    expand: ["latest_invoice.payment_intent"],
+  });
+  res.send(subscription);
+};
+
+module.exports = { createCustomer, createSubscription };
--- src/routes/payments.routes.js
+++ src/routes/payments.routes.js
@@ -3,6 +3,8 @@ const express = require("express");
 
 const router = express.Router();
 
+// POST /payments
 router.post("/create-payment-intent", paymentsController.createPaymentIntent);
+router.post("/create-subscription", paymentsController.createSubscription);
 
 module.exports = router;
